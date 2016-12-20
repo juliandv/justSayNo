f98549d7baa80ea8f6ce848aba526c88d6939384
juliandv <juliandv96@gmail.com>
2016-12-21 10:33:30 +1300

    testing logic for toggling between two excuses

--- js/script.js
+++ js/script.js
@@ -6,24 +6,23 @@ function readTextFile(file) {
                     if (rawFile.status === 200 || rawFile.status == 0) {
                         var allText = rawFile.responseText;
                         var excuseArr = allText.split('\n');
+                        var iNext;
                         console.log(excuseArr);
 
                         var i = 0;
                         initExcuse(0); //show the first photo initially
                         var excuseLength = excuseArr.length;
 
-                        $('#prev').click(function () {
-                            i--;
-                            if (i == -1) {
-                                initExcuse(excuseLength - 1);
-                                i = excuseLength - 1;
-                            } else {
-                                initExcuse(i);
-                            }
+                        $('#testBtn').click(function () {
                         });
 
-                        var nextBtnDiv = document.getElementById("nextBtn"); //grab the element
-                        nextBtnDiv.onclick = function () { //asign a function
+                        $('#testBtn2').click(function () {
+                            document.getElementById("excuse1").style.zIndex = "4";
+                            document.getElementById("excuse2").style.zIndex = "2";
+                            document.getElementById("excuseWrapper").style.zIndex = "3";
+                            document.getElementById("excuseBackground").style.zIndex = "3";
+                            document.getElementById("excuseBackground2").style.zIndex = "1";
+
                             i++;
                             if (i == excuseArr.length) {
                                 initExcuse(0);
@@ -31,15 +30,34 @@ function readTextFile(file) {
                             } else {
                                 initExcuse(i);
                             }
+
+                        });
+
+
+                        var nextBtnDiv = document.getElementById("nextBtn"); //grab the element
+                        nextBtnDiv.onclick = function () { //asign a function
+//                            i++;
+//                            if (i == excuseArr.length) {
+//                                initExcuse(0);
+//                                i = 0;
+//                            } else {
+//                                initExcuse(i);
+//                            }
                             $('.excuseWrapperNext').toggleClass('excuseAnimation');
 
                         }
 
                         function initExcuse(i) {
+                            iNext = i+1;
+
+                            if(iNext > excuseArr.length) {
+                                iNext = 0;
+                            }
+
                             $('#excuse1').empty();
-                            $('#excuse1').append(excuseArr[i]);
+                            $('#excuse1').append(excuseArr[(i+1)] + (i+1));
                             $('#excuse2').empty();
-                            $('#excuse2').append(excuseArr[(i+1)]);
+                            $('#excuse2').append(excuseArr[i] + i);
                         }
 
                     }
